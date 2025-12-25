8ffa295bc5cefa245600b943c72174ea6437cbb3
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2025-12-25 15:04:28 +0330

    fix bug: syntxt in Header

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function HeroSection() {
   return (
-    <div>HeroSection</div>
+    <div className='xl:max-w-7xl lg:max-w-5xl max-w-3xl mx-auto mt-20 bg-fuchsia-700'>
+
+        HeroSection
+
+    </div>
   )
 }
