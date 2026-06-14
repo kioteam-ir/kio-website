31eaf2d097c89fbcace0128dcf9110f118fbbbe1
pourya <pouryathesaymon@gmail.com>
2026-06-14 21:38:06 +0330

    second front-style task done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,17 @@
-import { Header } from '../components/Header';
-import HeroSection from '../components/HeroSection';
-import { MethodologySection } from '../components/MethodologySection';
-import ServicesSection from '../components/ServicesSection';
-import GrowthSection from '../components/GrowthSection';
-import ContactForm from '../components/ContactForm';
-import Footer from '../components/Footer';
-import Resumes from '../components/Resumes';
-import Consulting from '../components/Consulting';
+import { Header } from "../components/Header";
+import HeroSection from "../components/HeroSection";
+import { MethodologySection } from "../components/MethodologySection";
+import ServicesSection from "../components/ServicesSection";
+import GrowthSection from "../components/GrowthSection";
+import ContactForm from "../components/ContactForm";
+import Footer from "../components/Footer";
+import Resumes from "../components/Resumes";
+import Consulting from "../components/Consulting";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-900 text-white">
-      <div className="">
+      <div>
         <Header />
         <HeroSection />
         <MethodologySection />
