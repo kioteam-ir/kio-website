a20df26a5cc8ec867e07ecd3f23e1d4d240db42a
pourya <pouryathesaymon@gmail.com>
2026-07-23 15:53:14 +0330

    hero text added

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -8,15 +8,7 @@ export default function HeroSection() {
       className="mb-20 2xl:max-w-7xl xl:max-w-6xl lg:max-w-4xl md:max-w-2xl sm:max-w-xl max-w-lg mx-auto mt-7 md:mt-20 sm:px-0 px-2"
     >
       <div className="flex flex-col md:flex-row items-center justify-center md:justify-baseline lg:justify-between w-full xl:w-5/6 mx-auto">
-        <div className="">
-          <img
-            src="/404-computer.svg"
-            alt="web3-img"
-            className="w-full md:w-3/4 lg:w-full"
-          />
-        </div>
-
-        <div className="space-y-5 font-bold">
+        <div className="space-y-5 w-full font-bold">
           <div>
             <Highlight className="text-3xl sm:text-4xl md:text-3xl lg:text-5xl text-cyan-500">
               Kio Team
@@ -25,6 +17,16 @@ export default function HeroSection() {
               آینده رو بسازیم
             </h3>
           </div>
+          <p className="max-w-125 font-light text-[17px] leading-8">
+            تیم کایو ارائه دهنده خدمات مختلف در زمینه تولید نرم افزارهای تحت وب
+            و ارائه خدمات میزبانی می باشد. هدف اصلی تیم ایجاد و ارتقاء جایگاه
+            کسب و کار شما در دنیای وب می‌باشد و در این راستا راهکار های لازم را
+            با در نظر گرفتن استاندارد های روز ارائه می‌کند. ایجاد بستر مناسب
+            برای ورود یک کسب و کار در دنیای وب اولین قدم است و شما بدون داشتن
+            پشتیبان خوب مسیر دشواری را خواهید داشت. پشتیبانی به موقع و با کیفیت
+            یکی از مهمترین اهداف این مجموعه است و متخصصین ما همواره همراه شما
+            هستند تا با آسودگی خاطر کسب و کار خود را در دنیای وب گسترش دهید.
+          </p>
           <div className="pt-4 mt-10 lg:pt-7">
             <a
               href="#contact"
@@ -33,6 +35,13 @@ export default function HeroSection() {
               شروع کنید
             </a>
           </div>
+        </div>
+        <div className="mt-10">
+          <img
+            src="/404-computer.svg"
+            alt="web3-img"
+            className="w-full  md:w-3/4 lg:w-full"
+          />
         </div>
       </div>
     </div>
