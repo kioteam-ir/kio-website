2d29c1ce9c7512e0189135b2cc92aaa5ac629b90
pourya <pouryathesaymon@gmail.com>
2026-08-11 14:29:32 +0330

    feat: - blog api fully added [fetching list/deleteing email]

--- frontend/src/api/blogApi.js
+++ frontend/src/api/blogApi.js
@@ -1,6 +1,16 @@
 import { httpClient } from "../client/httpClient";
 
 export const blogApi = {
-  emailSubscription: (email) =>
-    httpClient.post("/api/front/blog/subscriptions/", { email: email }),
+  subscribeEmail: (email) =>
+    httpClient.post(
+      "/api/front/blog/subscriptions/",
+      { email },
+      { auth: false },
+    ),
+
+  listSubscriptions: ({ page = 1, size = 10 } = {}) =>
+    httpClient.post(`/api/admin/blog/subscriptions/?page=${page}&size=${size}`),
+
+  deleteSubscription: (id) =>
+    httpClient.delete(`/api/admin/blog/subscriptions/${id}`),
 };
